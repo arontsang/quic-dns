a5c8e9f88f9d2f53f85b9d348b1e6c84a03c1a5c
Aron Tsang <arontsang@gmail.com>
2025-12-22 23:05:40 +0800

    Use array pool on tcp

--- src/listener/tcp.rs
+++ src/listener/tcp.rs
@@ -1,20 +1,25 @@
+use crate::buffer::pooled_buf_mut::PooledBufMut;
 use crate::resolver::DnsResolver;
 use async_executor::LocalExecutor;
-use compio::buf::buf_try;
+use bytes::Bytes;
+use compio::buf::{buf_try, IntoInner, IoBuf};
 use compio::io::{AsyncReadExt, AsyncWriteExt};
 use compio::net::{TcpListener, TcpStream};
+use cyper::Body;
 use std::net::SocketAddr;
 use std::rc::Rc;
-use cyper::Body;
+use zeropool::{BufferPool, PooledBuffer};
 
-async fn do_work<T : DnsResolver + 'static>(resolver: Rc<T>, mut client: TcpStream) -> Result<(), std::io::Error>{
+async fn do_work<T : DnsResolver + 'static>(pool: &Rc<BufferPool>, resolver: Rc<T>, mut client: TcpStream) -> Result<(), std::io::Error>{
     let length = client.read_u16().await?;
     let length: usize = length.into();
 
-    let query = Vec::with_capacity(length);
-    let (_, query) = buf_try!(@try client.read_exact(query).await);
+    let query = pool.get(length);
+    let query = PooledBufMut::new(query);
+    let (_, query) = buf_try!(@try client.read_exact(query.slice(..length)).await);
 
-    let query = query.to_vec();
+    let query: PooledBuffer = query.into_inner().into();
+    let query = Bytes::from_owner(query);
     let query = Body::from(query);
     let response = resolver.resolve(query).await?;
 
@@ -30,15 +35,17 @@ pub async fn start<T : DnsResolver + 'static >(bind_address: SocketAddr, resolve
     let local_ex = Rc::new(LocalExecutor::new());
     local_ex.run(async {
         let local_ex = local_ex.clone();
+        let pool = Rc::new(BufferPool::new());
         loop {
 
             if let Ok((socket, _)) = listener.accept().await {
                 local_ex.spawn({
                     let resolver = resolver.clone();
+                    let mut pool = pool.clone();
                     async move {
                         // We don't care about success.
                         // TODO: Log Errors
-                        let _  = do_work(resolver, socket).await;
+                        let _  = do_work(&mut pool, resolver, socket).await;
                     }
                 }).detach();
             }
