b345e6a5a8760700cd0656670b6d3e00c5207c72
Aron Tsang <arontsang@gmail.com>
2026-01-03 18:55:57 +0800

    Try my branch

--- src/buffer/pooled_buf_mut.rs
+++ src/buffer/pooled_buf_mut.rs
@@ -1,4 +1,5 @@
-use compio::buf::{IoBuf, IoBufMut, SetLen};
+
+use compio::buf::{IoBuf, IoBufMut, IoSliceMut, SetBufInit};
 use std::mem::MaybeUninit;
 use zeropool::PooledBuffer;
 
@@ -12,8 +13,6 @@ impl PooledBufMut {
     pub fn new(buf: PooledBuffer) -> Self {
         Self{ buf }
     }
-
-
 }
 
 impl Into<PooledBuffer> for PooledBufMut {
@@ -24,22 +23,40 @@ impl Into<PooledBuffer> for PooledBufMut {
 
 
 
-impl IoBuf for PooledBufMut {
-    fn as_init(&self) -> &[u8] {
-        self.buf.as_init()
+unsafe impl IoBuf for PooledBufMut {
+    fn as_buf_ptr(&self) -> *const u8 {
+        self.buf.as_buf_ptr()
+    }
+
+    fn buf_len(&self) -> usize {
+        self.buf.buf_len()
+    }
+
+    fn buf_capacity(&self) -> usize {
+        self.buf.buf_capacity()
     }
 }
 
-impl SetLen for PooledBufMut {
-    unsafe fn set_len(&mut self, len: usize) {
-        unsafe{
-            self.buf.set_len(len)
+impl SetBufInit for PooledBufMut {
+    unsafe fn set_buf_init(&mut self, len: usize)  {
+        debug_assert!(len <= self.buf.len());
+    }
+}
+
+unsafe impl IoBufMut for PooledBufMut {
+    fn as_buf_mut_ptr(&mut self) -> *mut u8 {
+        self.buf.as_mut_ptr()
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [MaybeUninit<u8>] {
+        unsafe {
+            let slice =  self.buf.as_mut_slice();
+            std::slice::from_raw_parts_mut(slice.as_mut_ptr() as *mut MaybeUninit<u8>, slice.len())
         }
     }
-}
 
-impl IoBufMut for PooledBufMut {
-    fn as_uninit(&mut self) -> &mut [MaybeUninit<u8>] {
-        self.buf.as_uninit()
+    unsafe fn as_io_slice_mut(&mut self) -> IoSliceMut {
+        let slice =  self.buf.as_mut_slice();
+        unsafe { IoSliceMut::from_slice(slice) }
     }
 }
